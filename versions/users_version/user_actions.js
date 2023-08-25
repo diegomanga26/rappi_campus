46acbb6eea5762216935484ac88a4d26b47a2790
miusarname <oscarm.alvarezg@gmail.com>
2023-08-25 16:12:30 -0500

    docs: :memo: Documented the code in the corresponding readme files

--- versions/users_version/user_actions.js
+++ versions/users_version/user_actions.js
@@ -4,16 +4,19 @@ import { CURSOR_FLAGS } from "mongodb";
 
 // Función asincrónica para obtener el siguiente ID de una colección
 export async function siguienteId(coleccion) {
-  const db = await con();
-  const sequenceDocument = await db
-    .collection("counters")
-    .findOneAndUpdate(
-      { id: `${coleccion}Id` },
-      { $inc: { sequence_value: 1 } },
-      { returnDocument: "after" }
-    );
+  const db = await con(); // Conexión a la base de datos
+
+  // Busca el documento de secuencia correspondiente a la colección y actualiza su valor
+  const sequenceDocument = await db.collection("counters").findOneAndUpdate(
+    { id: `${coleccion}Id` }, // Filtro para encontrar el documento de secuencia
+    { $inc: { sequence_value: 1 } }, // Incrementa el valor de la secuencia en 1
+    { returnDocument: "after" } // Devuelve el documento actualizado
+  );
+
+  // Retorna el siguiente valor de la secuencia obtenido del documento actualizado
   return await sequenceDocument.value.sequence_value;
 }
+
 
 // Función para transformar las claves de un objeto
 function transformObject(inputObject, keyMap) {
@@ -34,12 +37,20 @@ function transformObject(inputObject, keyMap) {
   return transformedObject;
 }
 
-// Función para registrar un usuario
+// Función para registrar un nuevo usuario
 export async function registerUsuario(req, res) {
-  if (!req.rateLimit) return; // Si no se excede el límite de tasa, se retorna
-  const errors = validationResult(req); // Valida los errores de la solicitud
-  if (!errors.isEmpty())
-    return res.status(400).json({ errors: errors.array() }); // Si hay errores, se responde con 400
+  // Verifica si no se excede el límite de tasa, si es así, la función retorna
+  if (!req.rateLimit) return;
+
+  // Valida los errores de la solicitud usando express-validator
+  const errors = validationResult(req);
+
+  // Si hay errores de validación, responde con un estado 400 y la lista de errores en formato JSON
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // Desestructura los datos de la solicitud (nombre, correo, contraseña, etc.)
   const {
     name: Name,
     email: Mail,
@@ -47,8 +58,12 @@ export async function registerUsuario(req, res) {
     numCelular: CellPhone,
     address: Address,
     user_type: tipo_usuario,
-  } = req.body; // Desestructura los datos de la solicitud
-  const id = await siguienteId("usuario"); // Obtiene el siguiente ID
+  } = req.body;
+
+  // Obtiene el siguiente ID de usuario usando la función siguienteId y la colección "usuario"
+  const id = await siguienteId("usuario");
+
+  // Crea un objeto JSON con los datos del usuario
   const json = {
     nombre: Name,
     correo: Mail,
@@ -57,17 +72,26 @@ export async function registerUsuario(req, res) {
     direccion: Address,
     tipo_usuario,
     id,
-  }; // Crea un objeto JSON con los datos
+  };
 
   try {
-    const db = await con(); // Conexión a la base de datos
-    await db.collection("usuario").insertOne(json); // Inserta el usuario en la colección "usuario"
-    res.status(201).send("success"); // Responde con éxito
+    // Establece una conexión a la base de datos
+    const db = await con();
+
+    // Inserta el objeto JSON del usuario en la colección "usuario"
+    await db.collection("usuario").insertOne(json);
+
+    // Responde con un estado 201 (creado) y un mensaje de éxito
+    res.status(201).send("success");
   } catch (error) {
-    console.log(error, "error"); // Registra el error en la consola
-    res.status(500).send("error"); // Responde con error del servidor
+    // Si ocurre un error durante el proceso, registra el error en la consola
+    console.log(error, "error");
+
+    // Responde con un estado 500 (error interno del servidor) y un mensaje de error
+    res.status(500).send("error");
   }
 }
+
 
 // Función para iniciar sesión de un usuario
 export async function loginUsuario(req, res) {
@@ -148,19 +172,26 @@ export async function actualizarUsuario(req, res) {
 }
 
 // Esta función obtiene los pedidos realizados por un usuario según su ID.
+// Función para obtener los pedidos realizados por un usuario
 export async function verPedidosRealizadosUsuario(req, res) {
   try {
+    // Valida los errores de la solicitud usando express-validator
     const errors = validationResult(req);
+
+    // Si hay errores de validación, responde con un estado 400 y la lista de errores en formato JSON
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
+    // Establece una conexión a la base de datos
     const db = await con();
-    const usuario_id = req.usuario_id; // Asegúrate de obtener el usuario_id de alguna manera
+
+    // Obtiene el usuario_id de alguna manera (asegúrate de que está disponible en req.usuario_id)
+    const usuario_id = req.usuario_id;
+
+    // Realiza una agregación en la colección "ordenes" para obtener los detalles de los pedidos realizados por el usuario
     const user = await db.collection("ordenes").aggregate([
-      // Agregación para obtener los detalles de los pedidos realizados por el usuario
-      { $match: { user_id: usuario_id } },
-      // Realiza unión con la colección "carrito" y "usuarios" para obtener información adicional
+      { $match: { user_id: usuario_id } }, // Filtra las órdenes del usuario actual
       {
         $lookup: {
           from: "carrito",
@@ -168,8 +199,7 @@ export async function verPedidosRealizadosUsuario(req, res) {
           foreignField: "id",
           as: "cart",
         },
-      },
-      // Realiza unión con la colección "usuarios" para obtener información del repartidor
+      }, // Realiza unión con la colección "carrito" para obtener detalles del carrito
       {
         $lookup: {
           from: "usuarios",
@@ -177,8 +207,7 @@ export async function verPedidosRealizadosUsuario(req, res) {
           foreignField: "id",
           as: "repartidor",
         },
-      },
-      // Proyecta la información relevante
+      }, // Realiza unión con la colección "usuarios" para obtener información del repartidor
       {
         $project: {
           _id: 0,
@@ -192,35 +221,51 @@ export async function verPedidosRealizadosUsuario(req, res) {
           repartidor_nombre: "$repartidor.nombre",
           cart_content: "$cart.content_cart",
         },
-      },
+      }, // Proyecta la información relevante
     ]).toArray();
 
+    // Si se encontraron órdenes para el usuario, responde con un estado 200 y el primer resultado
     if (user.length > 0) {
       res.status(200).json({ message: "success", user: user[0] });
     } else {
+      // Si no se encontraron órdenes, responde con un estado 404 y un mensaje indicando que no se encontró el usuario
       res.status(404).json({ message: "user not found" });
     }
   } catch (error) {
+    // Si ocurre un error durante el proceso, registra el error en la consola
     console.log(error);
+
+    // Responde con un estado 500 (error interno del servidor) y un mensaje de error
     res.status(500).send("error");
   }
 }
 
+
 // Esta función obtiene las órdenes asignadas a un repartidor según su ID.
 export async function obtenerOrdenesPorRepartidor(req, res) {
   try {
-    if (!req.rateLimit) return; // Si no se excede el límite de tasa, se retorna
-    const errors = validationResult(req); // Valida los errores de la solicitud
-    if (!errors.isEmpty())
-      return res.status(400).json({ errors: errors.array() }); // Si hay errores, se responde con 400
+    // Si no se excede el límite de tasa, se retorna sin continuar
+    if (!req.rateLimit) return;
 
+    // Valida los errores de la solicitud utilizando express-validator
+    const errors = validationResult(req);
+
+    // Si hay errores de validación, responde con un estado 400 y la lista de errores en formato JSON
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Establecer una conexión a la base de datos
     const db = await con();
+
+    // Obtener el ID del repartidor desde los parámetros de la solicitud
     const repartidor_id = req.params.repartidor_id; // Asegúrate de obtener el repartidor_id de alguna manera
 
+    // Realizar una agregación para obtener las órdenes asignadas al repartidor
     const ordenes = await db.collection("ordenes").aggregate([
-      // Agregación para obtener las órdenes asignadas al repartidor
+      // Agregación para filtrar las órdenes por repartidor_id
       { $match: { repartidor_id: repartidor_id } },
-      // Realiza unión con las colecciones "carrito" y "usuarios" para obtener información adicional
+      // Realizar unión con las colecciones "carrito" y "usuarios" para obtener información adicional
       {
         $lookup: {
           from: "carrito",
@@ -237,7 +282,7 @@ export async function obtenerOrdenesPorRepartidor(req, res) {
           as: "usuario",
         },
       },
-      // Proyecta la información relevante
+      // Proyectar la información relevante para la respuesta
       {
         $project: {
           _id: 0,
@@ -254,22 +299,33 @@ export async function obtenerOrdenesPorRepartidor(req, res) {
       },
     ]).toArray();
 
+    // Responder con un estado 200 y un mensaje de éxito junto con las órdenes obtenidas en formato JSON
     res.status(200).json({ message: "success", ordenes });
   } catch (error) {
+    // Si ocurre un error durante el proceso, registrar el error en la consola
     console.log(error);
+
+    // Responder con un estado 500 (error interno del servidor) y un mensaje de error
     res.status(500).send("error");
   }
 }
 
+
 // Esta función actualiza la información de una orden según su ID.
 export async function actualizarOrden(req, res) {
-  if (!req.rateLimit) return; // Si no se excede el límite de tasa, se retorna
-  const errors = validationResult(req); // Valida los errores de la solicitud
-  if (!errors.isEmpty())
-    return res.status(400).json({ errors: errors.array() }); // Si hay errores, se responde con 400
+  // Si no se excede el límite de tasa, se retorna sin continuar
+  if (!req.rateLimit) return;
 
+  // Valida los errores de la solicitud utilizando express-validator
+  const errors = validationResult(req);
+
+  // Si hay errores de validación, responde con un estado 400 y la lista de errores en formato JSON
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // Mapeo de las claves del objeto en el cuerpo de la solicitud a las claves en la base de datos
   let mapper = {
-    // Mapeo de las claves del objeto en el cuerpo de la solicitud a las claves en la base de datos
     "id_cart": "cart_id",
     "id_user": "user_id",
     "dateTime": "fecha_hora",
@@ -279,17 +335,32 @@ export async function actualizarOrden(req, res) {
     "total": "total",
     "id_deliver": "repartidor_id"
   };
-  const json = transformObject(req.body, mapper); // Transformar las claves del objeto en el cuerpo de la solicitud
+
+  // Transformar las claves del objeto en el cuerpo de la solicitud utilizando la función transformObject
+  const json = transformObject(req.body, mapper);
+
+  // Obtener el ID de la orden desde los parámetros de la solicitud
   const _id = req.params.id;
   let id = parseInt(_id);
-  const filter = Object.assign({ id }); // Crear un filtro para buscar la orden por ID
+
+  // Crear un filtro para buscar la orden por ID
+  const filter = Object.assign({ id });
 
   try {
-    const db = await con(); // Conexión a la base de datos
-    const result = await db.collection("usuario").updateOne(filter, { $set: json }); // Actualizar la orden en la colección
-    res.status(200).json(result); // Responder con éxito y el resultado de la actualización
+    // Establecer una conexión a la base de datos
+    const db = await con();
+
+    // Actualizar la orden en la colección "usuario" utilizando la función updateOne
+    const result = await db.collection("usuario").updateOne(filter, { $set: json });
+
+    // Responder con un estado 200 y el resultado de la actualización en formato JSON
+    res.status(200).json(result);
   } catch (error) {
-    console.log(error, "error"); // Registrar el error en la consola
-    res.status(500).send("error"); // Responder con error del servidor
+    // Si ocurre un error durante el proceso, registrar el error en la consola
+    console.log(error, "error");
+
+    // Responder con un estado 500 (error interno del servidor) y un mensaje de error
+    res.status(500).send("error");
   }
 }
+
