a69249d52d1c1cfa7d9dfecdfa50eb4bc39257c5
miusarname <oscarm.alvarezg@gmail.com>
2023-08-25 12:48:52 -0500

    feat: :sparkles: The creation, design and structure of product management with its different routes is completed
    The development phase has been completed in which the conception, design and establishment of the integral structure for product management has been carried out, covering in detail the various paths or approaches that will be followed in its implementation and operation.

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { productsDto, productsDtoUpdate } from "../storage/products.dto.js";
-import { getAllProducts, createProduct, updateProducto } from "../versions/product_admin_version/product_actions.js";
+import { getAllProducts, createProduct, updateProducto, deleteProducto, getAllProductsWithOutCategory, getAllProductsWithOutAviality } from "../versions/product_admin_version/product_actions.js";
 
 export const products = Router();
 
@@ -9,4 +9,11 @@ products.get("/", getAllProducts);
 products.post('/', productsDto, createProduct)
 
 products.put('/:id', productsDtoUpdate, updateProducto)
+
+products.delete('/:id', deleteProducto)
+
+products.get('/:categoria', getAllProductsWithOutCategory)
+
+products.get('/disponibles', getAllProductsWithOutAviality)
+
 
--- versions/product_admin_version/product_actions.js
+++ versions/product_admin_version/product_actions.js
@@ -6,6 +6,7 @@ import { con } from "../../config/atlas.js";
 
 // Función para transformar las claves de un objeto
 function transformObject(inputObject) {
+  // Definimos un mapeo de claves entre las claves originales y las nuevas claves deseadas
   const keyMapping = {
     'name': 'nombre_producto',
     'description': 'descripcion',
@@ -14,16 +15,21 @@ function transformObject(inputObject) {
     'availability': 'disponibilidad',
   };
 
+  // Creamos un objeto vacío para almacenar las claves transformadas
   const transformedObject = {};
 
+  // Recorremos cada clave en el objeto de entrada
   for (const key in inputObject) {
     if (inputObject.hasOwnProperty(key)) {
+      // Si la clave existe en el mapeo, usamos la nueva clave; de lo contrario, usamos la clave original
       transformedObject[keyMapping[key] || key] = inputObject[key];
     }
   }
 
+  // Devolvemos el objeto transformado con las nuevas claves
   return transformedObject;
 }
+
 
 // Función para obtener todos los productos
 export async function getAllProducts(req, res) {
@@ -77,6 +83,65 @@ export async function updateProducto(req, res) {
     const db = await con(); // Conexión a la base de datos
     const result = await db.collection("productos").updateOne(filter, { $set: json }); // Actualizar el producto en la colección "productos"
     res.status(200).json(result); // Responder con éxito y el resultado de la actualización
+  } catch (error) {
+    console.log(error, "error"); // Registrar el error en la consola
+    res.status(500).send("error"); // Responder con error del servidor
+  }
+}
+
+// Función para eliminar un producto
+export async function deleteProducto(req, res) {
+  if (!req.rateLimit) return; // Si no se excede el límite de tasa, no se procede
+
+  const errors = validationResult(req); // Validar errores en la solicitud
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() }); // Si hay errores, responder con 400
+
+  const _id = req.params.id; // Obtenemos el ID del producto de los parámetros de la URL
+  let id = parseInt(_id); // Convertimos el ID a entero
+  const filter = Object.assign({ id }); // Creamos un filtro para buscar el producto por ID
+
+  try {
+    const db = await con(); // Conexión a la base de datos
+    const result = await db.collection("productos").updateOne(filter); // Actualizamos (eliminamos) el producto en la colección "productos"
+    res.status(200).json(result); // Responder con éxito y el resultado de la actualización (eliminación)
+  } catch (error) {
+    console.log(error, "error"); // Registrar el error en la consola
+    res.status(500).send("error"); // Responder con error del servidor
+  }
+}
+
+
+// Función para obtener todos los productos sin una categoría específica
+export async function getAllProductsWithOutCategory(req, res) {
+  if (!req.rateLimit) return; // Si no se excede el límite de tasa, no se procede
+
+  const errors = validationResult(req); // Validar errores en la solicitud
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() }); // Si hay errores, responder con 400
+  
+  try {
+    const db = await con(); // Conexión a la base de datos
+    const result = await db.collection("productos").find({ categoria: req.params.categoria }).toArray(); // Buscar documentos con la categoría especificada en los parámetros de la URL
+    res.status(200).json(result); // Responder con éxito y el arreglo de resultados
+  } catch (error) {
+    console.log(error, "error"); // Registrar el error en la consola
+    res.status(500).send("error"); // Responder con error del servidor
+  }
+}
+
+// Función para obtener todos los productos con disponibilidad
+export async function getAllProductsWithOutAviality(req, res) {
+  if (!req.rateLimit) return; // Si no se excede el límite de tasa, no se procede
+
+  const errors = validationResult(req); // Validar errores en la solicitud
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() }); // Si hay errores, responder con 400
+  
+  try {
+    const db = await con(); // Conexión a la base de datos
+    const result = await db.collection("productos").find({ disponibilidad: true }).toArray(); // Buscar documentos con disponibilidad true
+    res.status(200).json(result); // Responder con éxito y el arreglo de resultados
   } catch (error) {
     console.log(error, "error"); // Registrar el error en la consola
     res.status(500).send("error"); // Responder con error del servidor
