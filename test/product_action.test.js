a73cbd8304d163c8aef710011c87f5983639c8db
miusarname <oscarm.alvarezg@gmail.com>
2023-08-26 17:34:15 -0500

    test: :test_tube: A small check of the test was made.

--- test/product_action.test.js
+++ test/product_action.test.js
@@ -1,7 +1,34 @@
-function setNumber(setNumber) {
-    let a = setNumber;
-    return a;
-}
+const { validationResult } = require('express-validator')
+
+function transformObject(inputObject) {
+    const transformedObject = {};
+  
+    for (const key in inputObject) {
+      if (inputObject.hasOwnProperty(key)) {
+        let transformedKey;
+  
+        if (key === 'name') {
+          transformedKey = 'nombre';
+        } else if (key === 'email') {
+          transformedKey = 'correo';
+        } else if (key === 'password') {
+          transformedKey = 'contrasena';
+        } else if (key === 'numCelular') {
+          transformedKey = 'telefono';
+        } else if (key === 'address') {
+          transformedKey = 'direccion';
+        } else if (key === 'user_type') {
+          transformedKey = 'tipo_usuario';
+        } else {
+          transformedKey = key;
+        }
+  
+        transformedObject[transformedKey] = inputObject[key];
+      }
+    }
+  
+    return transformedObject;
+  }
 
 test('Debe enviar lo mismo que le digo',()=>{
     expect(1)
