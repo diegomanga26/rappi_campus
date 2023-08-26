9f576ffe49c8870e06ce0c173a2903a0462d27e8
miusarname <oscarm.alvarezg@gmail.com>
2023-08-25 20:17:13 -0500

    feat: :sparkles: The tasks of creating user routes are completed, in them the creation of order adjustments
    TODO: Document everything done, and refactor the code to obey the kiss methodology

--- storage/orden.dto.js
+++ storage/orden.dto.js
@@ -1,24 +1,29 @@
 import { check } from "express-validator";
 
-export async function orderDto(req,res) {
-    check("id_cart").isArray().notEmpty().withMessage("id_cart is required"),
-    check("id_user").isInt().notEmpty().withMessage("id_user is required"),
-    check("dateTime").isDate().notEmpty().withMessage("dateTime is required"),
-    check("adress_to_send").isString().notEmpty().withMessage("adress_to_send is required"),
-    check("status").isString().notEmpty().withMessage("status is required"),
-    check("detalles_pago").isString().notEmpty().withMessage("detalles_pago is required"),
-    check("total").isNumeric().notEmpty().withMessage("total is required"),
-    check("id_deliver").isInt().notEmpty().withMessage("id_deliver is required")
-}
+export const orderDto = [
+  check("id_cart").isArray().notEmpty().withMessage("id_cart is required"),
+  check("id_user").isInt().notEmpty().withMessage("id_user is required"),
+  check("dateTime").isDate().notEmpty().withMessage("dateTime is required"),
+  check("adress_to_send")
+    .isString()
+    .notEmpty()
+    .withMessage("adress_to_send is required"),
+  check("status").isString().notEmpty().withMessage("status is required"),
+  check("detalles_pago")
+    .isString()
+    .notEmpty()
+    .withMessage("detalles_pago is required"),
+  check("total").isNumeric().notEmpty().withMessage("total is required"),
+  check("id_deliver").isInt().notEmpty().withMessage("id_deliver is required"),
+];
 
-
-export async function orderDtoUpdate(req,res) {
-    check("id_cart").isArray().withMessage("id_cart is required"),
-    check("id_user").isInt().withMessage("id_user is required"),
-    check("dateTime").isDate().withMessage("dateTime is required"),
-    check("adress_to_send").isString().withMessage("adress_to_send is required"),
-    check("status").isString().withMessage("status is required"),
-    check("detalles_pago").isString().withMessage("detalles_pago is required"),
-    check("total").isNumeric().withMessage("total is required"),
-    check("id_deliver").isInt().withMessage("id_deliver is required")
-}
+export const orderDtoUpdate = [
+  check("id_cart").isArray().withMessage("id_cart is required"),
+  check("id_user").isInt().withMessage("id_user is required"),
+  check("dateTime").isDate().withMessage("dateTime is required"),
+  check("adress_to_send").isString().withMessage("adress_to_send is required"),
+  check("status").isString().withMessage("status is required"),
+  check("detalles_pago").isString().withMessage("detalles_pago is required"),
+  check("total").isNumeric().withMessage("total is required"),
+  check("id_deliver").isInt().withMessage("id_deliver is required"),
+];
