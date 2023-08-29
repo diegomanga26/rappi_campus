0e614e2840f1b9323023e26cbacde851d9d62476
miusarname <oscarm.alvarezg@gmail.com>
2023-08-28 23:27:34 -0500

    test: :test_tube: stuck on tests
    continue until everything is pass

--- test/user_actions.test.js
+++ test/user_actions.test.js
@@ -1,7 +1,5 @@
 import { validationResult } from "express-validator";
 import { con } from "../config/atlas.js";
-
-// Importa las funciones a testear
 import {
   actualizarOrden,
   actualizarUsuario,
@@ -14,106 +12,104 @@ import {
   loginUsuario
 } from "../versions/users_version/user_actions.js";
 
-// Función para testear `siguienteId()`
+// Mock req and res objects for testing
+const mockReq = { rateLimit: true, body: {} };
+const mockRes = {
+  status: function (statusCode) {
+    this.statusCode = statusCode;
+    return this;
+  },
+  json: function (data) {
+    this.data = data;
+    return this;
+  },
+  send: function (message) {
+    this.message = message;
+    return this;
+  }
+};
+
 it("Devuelve el siguiente ID de una colección", async () => {
   const id = await siguienteId("usuario");
-  expect(id).toBe(id);
+  expect(typeof id).toBe("number");
 });
 
-// Función para testear `registerUsuario()`
 it("Registra un nuevo usuario correctamente", async () => {
-  const body = {
+  mockReq.body = {
     name: "John Doe",
     email: "johndoe@example.com",
     password: "password",
-    numCelular : "123",
-    address:'a',
-    user_type:"cliente"
+    numCelular: "123",
+    address: 'a',
+    user_type: "cliente"
   };
-  const errors = validationResult(body);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
-  }
+  const errors = validationResult(mockReq);
   expect(errors.isEmpty()).toBe(true);
 
-  const result = await registerUsuario(body);
-  expect(result).toBe({status : 201, message :"success"});
+  const result = await registerUsuario(mockReq, mockRes);
+  expect(result.status).toBe(201);
+  expect(result.message).toBe("success");
 });
 
 it("No registra un usuario con un correo electrónico duplicado", async () => {
-  const body = {
+  mockReq.body = {
     name: "John Doe1",
     email: "johndoe@example.com",
     password: "password",
-    numCelular : "123",
-    address:'a',
-    user_type:"cliente"
+    numCelular: "123",
+    address: 'a',
+    user_type: "cliente"
   };
-  const errors = validationResult(body);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
-  }
+  const errors = validationResult(mockReq);
   expect(errors.isEmpty()).toBe(true);
 
-  await registerUsuario(body);
-
-  const result = await registerUsuario(body);
-  expect(result).toBe(400);
+  const result = await registerUsuario(mockReq, mockRes);
+  expect(result.statusCode).toBe(400);
 });
 
 it("No registra un usuario con una contraseña demasiado corta", async () => {
-  const body = {
+  mockReq.body = {
     name: "John Doe",
     email: "johndoe@example.com",
     password: "pass",
   };
-  const errors = validationResult(body);
+  const errors = validationResult(mockReq);
   expect(errors.isEmpty()).toBe(false);
   expect(errors.array()[0].param).toBe("password");
-  expect(errors.array()[0].message).toBe(
+  expect(errors.array()[0].msg).toBe(
     "La contraseña debe tener al menos 8 caracteres"
   );
 });
 
-// Función para testear `loginUsuario()`
 it("Inicia sesión de un usuario correctamente", async () => {
-  const body = {
+  mockReq.body = {
     email: "johndoe@example.com",
     password: "password",
   };
-  const errors = validationResult(body);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
-  }
+  const errors = validationResult(mockReq);
   expect(errors.isEmpty()).toBe(true);
 
-  const result = await loginUsuario(body);
-  expect(result).toBe(200);
+  const result = await loginUsuario(mockReq, mockRes);
+  expect(result.statusCode).toBe(200);
 });
 
 it("No inicia sesión de un usuario con un correo electrónico incorrecto", async () => {
-  const body = {
+  mockReq.body = {
     email: "johndoe@example.com",
     password: "password",
   };
-  const errors = validationResult(body);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
-  }
+  const errors = validationResult(mockReq);
   expect(errors.isEmpty()).toBe(true);
 
-  const result = await loginUsuario(body);
-  expect(result).toBe(404);
+  const result = await loginUsuario(mockReq, mockRes);
+  expect(result.statusCode).toBe(404);
 });
 
 it("No inicia sesión de un usuario con una contraseña incorrecta", async () => {
-  const body = {
+  mockReq.body = {
     email: "johndoe@example.com",
     password: "password",
   };
-  const errors = validationResult(body);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
-  }
+  const errors = validationResult(mockReq);
   expect(errors.isEmpty()).toBe(true);
-});
+},10000);
