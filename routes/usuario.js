70892638a286342e98186ba4bac75b7f430c9ccb
diegomanga26 <diegoalexandermanga@gmail.com>
2023-09-04 13:16:06 -0500

    style: :truck: Changing the export type.
    The export type of the app constants has been changed, since the endpoint generator did not detect them.

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,12 +3,12 @@ import { orderDto, orderDtoUpdate } from "../storage/orden.dto.js";
 import { updateUserDto } from "../storage/user.dto.js";
 import routesVersioning from "express-routes-versioning";
 import {
-  actualizarOrden,
-  actualizarUsuario,
-  crearOrden,
-  obtenerInfoUsuario,
-  obtenerOrdenesPorRepartidor,
-  verPedidosRealizadosUsuario,
+    actualizarOrden,
+    actualizarUsuario,
+    crearOrden,
+    obtenerInfoUsuario,
+    obtenerOrdenesPorRepartidor,
+    verPedidosRealizadosUsuario,
 } from "../versions/users_version/user_actions.js";
 import { tokenValidate } from "../middlewares/JWT.js";
 
@@ -17,43 +17,45 @@ const version = routesVersioning();
 appUsuario.use(tokenValidate);
 
 appUsuario.get(
-  "/cliente/:id",
-  version({
+    "/cliente/:id",
+    version({
     "3.0.0": obtenerInfoUsuario,
-  })
+    })    
 );
 appUsuario.get(
-  "/repartidor/:id",
-  version({
-    "3.0.0": obtenerInfoUsuario,
-  })
+    "/repartidor/:id",
+    version({
+        "3.0.0": obtenerInfoUsuario,
+    })
 );
 appUsuario.put(
-  "/repartidor/:id",
-  updateUserDto,
-  version({
-    "3.0.0": actualizarUsuario,
-  })
+    "/repartidor/:id",
+    updateUserDto,
+    version({
+        "3.0.0": actualizarUsuario,
+    })
 );
 appUsuario.put(
-  "/cliente/:id",
-  updateUserDto,
-  version({
-    "3.0.0": actualizarUsuario,
-  })
+    "/cliente/:id",
+    updateUserDto,
+    version({
+        "3.0.0": actualizarUsuario,
+    })
 );
 appUsuario.get(
-  "/cliente/pedidos/:id",
-  version({
-    "3.0.0": verPedidosRealizadosUsuario,
-  })
+    "/cliente/pedidos/:id",
+    version({
+        "3.0.0": verPedidosRealizadosUsuario,
+    })
 );
 appUsuario.get("/repartidor/entregas/:id",   version({
     "3.0.0": obtenerOrdenesPorRepartidor,
-  }));
+    }));
 appUsuario.put("/cliente/pedidos/:id", orderDtoUpdate, version({
     "3.0.0": actualizarOrden,
-  }));
+    }));  
 appUsuario.post("/cliente/pedido", orderDto, version({
     "3.0.0": crearOrden,
-  }));
+    }));
+    
+export default appUsuario;
