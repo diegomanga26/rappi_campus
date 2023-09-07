0fde833d32f7b49de80cf36c9f75e4fe0b8c0efd
diegomanga26 <diegoalexandermanga@gmail.com>
2023-09-07 15:26:02 -0500

    feat: :safety_vest: Updating DTO.
    DTOs are updated for correct data entry.

--- storage/door.dto.js
+++ storage/door.dto.js
@@ -3,10 +3,10 @@ import { check } from "express-validator";
 // Definición de validaciones DTO para el registro de usuario
 export const registerDto = [
     check("name").notEmpty().isString().withMessage("name is required"),
+    check("address").notEmpty().isString().withMessage("address is required"),
+    check("numCelular").notEmpty().isString().withMessage("numCelular is required"),
     check("email").notEmpty().isString().withMessage("email is required"),
     check("password").notEmpty().isString().withMessage("password is required"),
-    check("numCelular").notEmpty().isString().withMessage("numCelular is required"),
-    check("address").notEmpty().isString().withMessage("address is required"),
     check("user_type").notEmpty().isString().withMessage("user_type is required"),
     (req, res, next) => {
         if (req.body.user_type === 'repartidor') {
